ae3b5982301a2a7d286bbb927afe106f914a63cf
Prodeep Ghosh <prodeep.ghosh.ext@zeiss.com>
2024-02-08 16:17:50 +0530

    introduced mock methods

--- ClimateSystem/OutsideTempSensor.cpp
+++ ClimateSystem/OutsideTempSensor.cpp
@@ -1,6 +1,6 @@
 #include "OutsideTempSensor.h"
 
 double OutsideTempSensor::getOutsideTemperature() {
-    this->outsideTemperature = 3;
+    this->outsideTemperature = 25.0;
     return this->outsideTemperature;
 }
